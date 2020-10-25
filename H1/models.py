e1ffdb2567ebc283216bb0e3471a853e1921dc7a
Erick Maraz <erickmaraz@ericks-macbook-pro.home>
2020-10-25 09:35:04 +0100

    Add Newtons Method

--- H1/models.py
+++ H1/models.py
@@ -11,6 +11,12 @@ def softmax(z):
     sum = 1 / np.sum(np.exp(z), axis=1).reshape(-1,1)
     return np.exp(z)*sum
 
+def softmax2(z, y):
+    '''
+    Calculate the softmax function with the pointwise product of y: R^K x R^K -> R.
+    '''
+    return (softmax2 * y).sum(axis=1)
+
 def compute_loss(y, tx, theta):
     '''
     Compute the loss L: R^(NxK) x R^(NxD) x R^(DxK) -> R.
@@ -29,6 +35,40 @@ def compute_gradient(y, tx, theta):
     gradient = gradient + tx.T @ (softmax(z) * y)
     return gradient
 
+def compute_gradient2(y, tx, theta):
+    '''
+    Compute the gradient (stacked weights in a vector).
+    '''
+    K = theta.shape[1]
+    z = tx @ theta
+    tx_tilde = np.kron(np.eye(K), tx) # Kronecker product
+    y_tilde = y.T.reshape(-1,1).squeeze()
+    p_tilde = softmax(z).T.reshape(-1,1).squeeze()
+
+    gradient = -tx_tilde.T @ (y_tilde - p_tilde)
+    return gradient
+
+def compute_hessian2(y, tx, theta):
+    '''
+    Compute the hessian (stacked weights in a vector).
+    '''
+    z = tx @ theta
+    N = tx.shape[0]
+    K = theta.shape[1]
+    p = softmax(z)*y
+    
+    tx_tilde = np.kron(np.eye(K), tx) # Kronecker product
+    W_tilde = np.zeros((N*K, N*K))
+    for i in range(K):
+        for j in range(K):
+            if i == j:
+                np.fill_diagonal(W_tilde[i*N:(i+1)*N, j*K:(j+1)*K], p[:,i]*(1-p[:,j]))
+            else:
+                np.fill_diagonal(W_tilde[i*N:(i+1)*N, j*K:(j+1)*K], -p[:,i]*p[:,j])
+
+    hessian = tx_tilde.T @ W_tilde @ tx_tilde 
+    return hessian
+
 def compute_acc(y, tx, theta):
     '''
     Compute the accuracy: good_predictions over all the predictions.
@@ -37,7 +77,7 @@ def compute_acc(y, tx, theta):
     output = np.argmax(softmax(z), axis=1)
     return np.sum(output == np.argmax(y, axis=1))/len(y)
 
-def gradient_descent(y_train, x_train, y_test, x_test, theta_0, max_iters, alpha, print_res_each=10):
+def gradient_descent(y_train, x_train, y_test, x_test, theta_0, max_iters, alpha, print_res_each=10, stacked=False):
     '''
     Gradient descent.
     '''
@@ -78,5 +118,54 @@ def stochastic_gradient_descent(y_train, x_train, y_test, x_test, theta_0, max_i
                     loss_test,compute_acc(y_test, x_test, theta)))
             gradient = compute_gradient(y_batch, x_batch, theta) # Calculate the gradient
             theta = theta - alpha*gradient # Update the gradient
+    print('')
+    return losses_train, losses_test, thetas_s
+
+# Stacked weights methods
+
+def gradient_descent2(y_train, x_train, y_test, x_test, theta_0, max_iters, alpha, print_res_each=10):
+    '''
+    Gradient descent with stacked weights.
+    '''
+    thetas_s, losses_train, losses_test = [], [], [] # store values to plot and display results
+    theta = theta_0
+    for n_iter in range(max_iters):
+        loss_train = compute_loss(y_train, x_train, theta)
+        loss_test = compute_loss(y_test, x_test, theta)
+        losses_train.append(loss_train) # store train losses
+        losses_test.append(loss_test) # store test losses
+        thetas_s.append(theta) # store thetas
+        if np.mod(n_iter+1,print_res_each) == 0:
+            print('iter : {}/{} - train_loss = {:0.2f}, train_acc = {:0.2f}, test_loss = {:0.2f}, test_acc = {:0.2f}'.format(
+                n_iter+1, max_iters ,loss_train,compute_acc(y_train, x_train, theta),
+                loss_test,compute_acc(y_test, x_test, theta)))
+        gradient = compute_gradient2(y_train, x_train, theta) # Calculate the gradient stacked
+        theta = theta.T.reshape(-1,1).squeeze() # Squeeze the weights
+        theta = theta - alpha*gradient # Update the gradient
+        theta = theta.reshape(theta_0.T.shape).T # Unsqueezer the weights
+    print('')
+    return losses_train, losses_test, thetas_s
+
+def newtons_method2(y_train, x_train, y_test, x_test, theta_0, max_iters, alpha, print_res_each=10):
+    '''
+    Newtons method with stacked weights.
+    '''
+    thetas_s, losses_train, losses_test = [], [], [] # store values to plot and display results
+    theta = theta_0
+    for n_iter in range(max_iters):
+        loss_train = compute_loss(y_train, x_train, theta)
+        loss_test = compute_loss(y_test, x_test, theta)
+        losses_train.append(loss_train) # store train losses
+        losses_test.append(loss_test) # store test losses
+        thetas_s.append(theta) # store thetas
+        if np.mod(n_iter+1,print_res_each) == 0:
+            print('iter : {}/{} - train_loss = {:0.2f}, train_acc = {:0.2f}, test_loss = {:0.2f}, test_acc = {:0.2f}'.format(
+                n_iter+1, max_iters ,loss_train,compute_acc(y_train, x_train, theta),
+                loss_test,compute_acc(y_test, x_test, theta)))
+        gradient = compute_gradient2(y_train, x_train, theta) # Calculate the gradient stacked
+        hessian = compute_hessian2(y_train, x_train, theta)
+        theta = theta.T.reshape(-1,1).squeeze() # Squeeze the weights
+        theta = theta - alpha*(np.linalg.pinv(hessian) @ gradient) # Update the gradient
+        theta = theta.reshape(theta_0.T.shape).T # Unsqueezer the weights
     print('')
     return losses_train, losses_test, thetas_s
--- H1/utils.py
+++ H1/utils.py
@@ -34,3 +34,14 @@ def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
         if start_index != end_index:
             yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
 
+def corr2_coeff(A, B):
+    # Rowwise mean of input arrays & subtract from input arrays themeselves
+    A_mA = A - A.mean(1)[:, None]
+    B_mB = B - B.mean(1)[:, None]
+
+    # Sum of squares across rows
+    ssA = (A_mA**2).sum(1)
+    ssB = (B_mB**2).sum(1)
+
+    # Finally get corr coeff
+    return np.dot(A_mA, B_mB.T) / np.sqrt(np.dot(ssA[:, None],ssB[None]))
